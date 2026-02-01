9cb3bae6ccb8f630ea04c703479071ce50403e1e
Mubarak <adebowaleraji0103@gmail.com>
2026-02-01 23:41:39 +0100

    djn

--- components/Header/Mobile.tsx
+++ components/Header/Mobile.tsx
@@ -1,5 +1,15 @@
 export default function Mobile() {
   return (
-    <div>Mobile</div>
-  )
+      <section className="bg-primary-dark text-neutral-white flex justify-between items-center py-5 px-7 w-full ">
+        <div className="flex gap-4 items-center">
+          <img src="header-logo.PNG" alt="" className="object-contain size-8" />
+          <h1 className="">HomePro</h1>
+        </div>
+        <img
+          src="menu.PNG"
+          alt=""
+          className="object-contain size-8 lg:hidden"
+        />
+      </section>
+  );
 }
--- components/Header/Navbar.tsx
+++ components/Header/Navbar.tsx
@@ -3,17 +3,7 @@ import Desktop from "./Desktop";
 export default function Navbar() {
   return (
     <header className="">
-      <section className="bg-primary-dark text-neutral-white flex justify-between items-center py-5 px-7 w-full ">
-        <div className="flex gap-4 items-center">
-          <img src="header-logo.PNG" alt="" className="object-contain size-8" />
-          <h1 className="">HomePro</h1>
-        </div>
-        <img
-          src="menu.PNG"
-          alt=""
-          className="object-contain size-8 lg:hidden"
-        />
-      </section>
+     
       <Desktop />
     </header>
   );
