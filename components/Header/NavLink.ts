80516115e6dd841ba65d533ded0c68fc639288e8
Mubarak <adebowaleraji0103@gmail.com>
2026-02-02 00:02:55 +0100

    dkd

--- components/Header/NavLink.ts
+++ components/Header/NavLink.ts
@@ -1,6 +1,6 @@
 export const NavLink = [
   { href: "/about", label: "About US" },
-  { href: "/about", label: "About US" },
-  { href: "/about", label: "About US" },
+  { href: "/service", label: "Services" },
+  { href: "/blog", label: "Our Blog" },
   { href: "/about", label: "About US" },
 ];
