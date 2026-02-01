67be302c33c9afb397bef9c6ccc1473ad66a877d
Mubarak <adebowaleraji0103@gmail.com>
2026-02-02 00:07:19 +0100

    lv

--- components/Header/Desktop.tsx
+++ components/Header/Desktop.tsx
@@ -1,16 +1,13 @@
+import { NavLink } from "./NavLink";
 
 export default function Desktop() {
   return (
     <div className="hidden lg:flex">
-      {/* {NavLink.map((card, index) => (
-        <ul key={index} className="flex gap-7">
-          <li>{card.about}</li>
-          <li>{card.service}</li>
-          <li>{card.blog}</li>
-          <li>{card.contact}</li>
-        </ul>
-      ))} */}
+      {NavLink.map((card, index) => (
+        <div key={index} className=" gap-7">
+            <a href={card.href}>{card.label}</a>
+        </div>
+      ))}
     </div>
   );
-  
 }
--- components/Header/NavLink.ts
+++ components/Header/NavLink.ts
@@ -2,5 +2,5 @@ export const NavLink = [
   { href: "/about", label: "About US" },
   { href: "/service", label: "Services" },
   { href: "/blog", label: "Our Blog" },
-  { href: "/about", label: "About US" },
+  { href: "/contact", label: "Our Contact" },
 ];
