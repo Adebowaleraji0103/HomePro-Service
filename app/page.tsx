62f74f955c8fe50263b08975341b6d95dd68cf56
Mubarak <adebowaleraji0103@gmail.com>
2026-02-05 15:36:42 +0100

    md

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "@/components/Header/Navbar";
+import HeroSection from "@/components/Home/HeroSection";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <HeroSection/>
     </main>
   );
 }
---
+++ components/Home/HeroSection.tsx
@@ -0,0 +1,11 @@
+export default function HeroSection() {
+  return (
+    <section className="bg-primary-dark text-foreground">
+      <div className="flex justify-between py-5 px-5">
+        <p>Maintenances</p>
+        <p>Repairs </p>
+        <p>Improvements</p>
+      </div>
+    </section>
+  );
+}
