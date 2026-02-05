90d6687b701c7d43c6e91fcb4be0f9bad632383a
Mubarak <adebowaleraji0103@gmail.com>
2026-02-05 13:40:32 +0100

    d

--- components/Header/Desktop.tsx
+++ components/Header/Desktop.tsx
@@ -1,13 +1,28 @@
 import { NavLink } from "./Navbar";
+import { Logo } from "@/app/card";
 
 export default function Desktop() {
   return (
-    <div className="hidden lg:flex">
-      {NavLink.map((card, index) => (
-        <div key={index} className=" gap-7">
+    <section className="hidden lg:flex">
+      <div>
+        {Logo.map((card, index) => (
+          <div key={index} className="flex gap-4 items-center">
+            <img src={card.image} alt="" className="object-contain size-8" />
+            <h1> {card.name}</h1>
+          </div>
+        ))}
+      </div>
+      <div>
+        {NavLink.map((card, index) => (
+          <div key={index} className=" gap-7">
             <a href={card.href}>{card.label}</a>
-        </div>
-      ))}
-    </div>
+          </div>
+        ))}
+      </div>
+      <div>
+        <p className="w-5 h-5 rounded-full bg-[#FF5E5E]"></p>
+        <p>24 Hour Services</p>
+      </div>
+    </section>
   );
 }
