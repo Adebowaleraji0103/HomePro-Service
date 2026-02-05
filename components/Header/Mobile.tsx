eb429c85e65514fa2726d8147c8a006e2ef3b2de
Mubarak <adebowaleraji0103@gmail.com>
2026-02-05 13:00:24 +0100

    djf

--- components/Header/Mobile.tsx
+++ components/Header/Mobile.tsx
@@ -3,12 +3,8 @@ import { Logo } from "@/app/card";
 export default function Mobile() {
   return (
     <section className="flex justify-between items-center  lg:hidden">
-      {/* <div className="flex gap-4 items-center">
-          <img src="header-logo.PNG" alt="" className="object-contain size-8" />
-          <h1>HomePro</h1>
-        </div> */}
       {Logo.map((card, index) => (
-        <div key={index}>
+        <div key={index} className="flex gap-4 items-center">
           <img src={card.image} alt="" />
           <h1> {card.name}</h1>
         </div>
