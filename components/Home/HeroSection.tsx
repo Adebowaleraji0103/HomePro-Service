cc2c2a3d6bfb9b6f173fe913ada5384d6d752059
Mubarak <adebowaleraji0103@gmail.com>
2026-02-05 15:49:34 +0100

    djf

--- components/Home/HeroSection.tsx
+++ components/Home/HeroSection.tsx
@@ -1,10 +1,11 @@
 export default function HeroSection() {
   return (
     <section className="bg-primary-dark text-foreground">
-      <div className="flex justify-between items-center py-5 px-5">
+      <div className="flex justify-between items-center px-5">
         <p>Maintenances</p>
-        <p className="h-3 w-3 rounded-full bg-red-300"></p>
+        <p className="h-1 w-1 rounded-full bg-red-300 border-none"></p>
         <p>Repairs </p>
+        <p className="h-1 w-1 rounded-full bg-red-300 border-none"></p>
         <p>Improvements</p>
       </div>
     </section>
