660b7cbd33ea48cdcf768df844dde2b6bf449217
Mubarak <adebowaleraji0103@gmail.com>
2026-02-05 16:13:21 +0100

    dj

--- components/Home/HeroSection.tsx
+++ components/Home/HeroSection.tsx
@@ -1,13 +1,14 @@
 export default function HeroSection() {
   return (
-    <section className="bg-primary-dark text-foreground">
-      <div className="flex justify-between items-center text-neutral-grey-2 px-5">
+    <section className="bg-primary-dark text-foreground text-center px-5">
+      <div className="flex justify-between items-center text-neutral-grey-2 ">
         <p>Maintenances</p>
         <p className="h-1 w-1 rounded-full bg-neutral-grey-2 border-none"></p>
         <p>Repairs </p>
         <p className="h-1 w-1 rounded-full bg-neutral-grey-2 border-none"></p>
         <p>Improvements</p>
       </div>
+      <h1>Need improvement or repair your home? we can help!</h1>
     </section>
   );
 }
